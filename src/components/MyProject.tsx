bd4419da5120461d3e8f1bb3b6bc1412667d5db6
Kirill Ibragimov <kirillibragimov1221@gmail.com>
2023-03-25 02:19:05 +0400

    Load user project data from API

--- src/components/MyProject.tsx
+++ src/components/MyProject.tsx
@@ -1,22 +1,41 @@
 import { Box, Grid, Typography } from '@mui/material'
 import Image from 'next/image'
 
-const MyProject = () => {
+type Props = {
+  project: {
+    title: string
+    avatar: string | null
+  }
+}
+
+const MyProject = (props: Props) => {
+  const { project } = props
+
   return (
-    <Box sx={{ border: '1px solid #DDDEE0', borderRadius: '16px', p: '16px' }}>
-      <Grid container spacing={3} sx={{ height: '88px' }}>
-        <Grid item xs>
-          <Image src="/demo/image 14.png" width={64} height={64} alt={'logo'} />
-        </Grid>
-        <Grid item xs={10} sx={{ pl: '10px !important' }}>
+    <Box
+      sx={{
+        border: '1px solid #DDDEE0',
+        borderRadius: '16px',
+        p: '16px',
+        height: '98px',
+        mb: '24px',
+      }}
+    >
+      <div style={{ display: 'flex', flexWrap: 'nowrap' }}>
+        {project.avatar && (
+          <div style={{ display: 'inline-block', paddingRight: '24px' }}>
+            <Image src={project.avatar} width={64} height={64} alt={'logo'} />
+          </div>
+        )}
+        <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
           <Typography sx={{ fontSize: '18px', fontWeight: '600', lineHeight: '31px' }}>
             Generate new gameplay ad concepts
           </Typography>
           <Typography sx={{ fontSize: '18px', fontWeight: '600', lineHeight: '31px' }}>
-            for Animal Kingdom
+            for {project.title}
           </Typography>
-        </Grid>
-      </Grid>
+        </div>
+      </div>
     </Box>
   )
 }
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,5 +2,9 @@ const endpoints = {
   meEndpoint: '/auth/me',
   loginEndpoint: '/auth/login',
   logoutEndpoint: '/auth/logout',
+
+  profileEndpoint: '/user/profile',
+  ideaGenerateEndpoint: '/concepts/idea',
+  conceptGenerateEndpoint: '/concepts/generate',
 }
 export default endpoints
